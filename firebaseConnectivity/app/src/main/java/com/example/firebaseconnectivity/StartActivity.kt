bad185cd4d8f2a4821c839fbff6986c2a28e3be1
Sharmaaryan555 <Sharmaaryan55555@gmail.com>
2024-04-22 19:46:24 +0530

    Authentication Completed

--- firebaseConnectivity/app/src/main/java/com/example/firebaseconnectivity/StartActivity.kt
+++ firebaseConnectivity/app/src/main/java/com/example/firebaseconnectivity/StartActivity.kt
@@ -3,17 +3,16 @@ package com.example.firebaseconnectivity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
 
     lateinit var  register:Button;
      lateinit var login:Button;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
 
         register = findViewById<Button>(R.id.register)
         login = findViewById<Button>(R.id.login)
